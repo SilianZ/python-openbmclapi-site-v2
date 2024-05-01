57c44212ea7bbefb7dbdba4dbc08813c05d10da4
SilianZ <silian_zheng@outlook.com>
2024-05-01 19:29:35 +0800

    :sparkles: 更新文档

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,7 +103,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.okaidia,
-        additionalLanguages: ['bash', 'diff', 'json', 'python', 'powershell', 'yaml']
+        additionalLanguages: ['bash', 'diff', 'json', 'python', 'powershell', 'yaml', 'properties']
       },
       algolia: {
         appId: '6DIHTRUAUN',
