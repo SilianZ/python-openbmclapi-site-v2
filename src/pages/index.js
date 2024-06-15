26bc6dfbe213d2afb9cf6e43e63b2d3692353469
bugungu <113701655+SilianZ@users.noreply.github.com>
2024-06-15 13:08:25 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function HomePage() {
         <div className={styles.heroInner}>
           <Heading as="h1" className={styles.heroProjectTagline}>
             <img
-              width="300"
+              width="250"
               className={styles.heroLogo}
               src="./img/logo.svg"
               alt="logo"
