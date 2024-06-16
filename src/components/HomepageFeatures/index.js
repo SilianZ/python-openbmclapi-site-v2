35c262abd3140ab466de5ccacad9e3e9879a4ae2
bugungu <113701655+SilianZ@users.noreply.github.com>
2024-06-16 16:39:18 +0800

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,40 +1,31 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
-import translate from '@docusaurus/Translate'
+import Translate, { translate } from '@docusaurus/Translate'
 
 const FeatureList = [
   {
     title: translate({ message: '轻易上手', id: 'homepage.feature.easy-to-use.title' }),
     description: (
-      <>
-        translate({
-          message: '为了更快地开发和部署，我们准备了文档以及 Docker 镜像，同时也搭建了相应的镜像站来加快访问速度。',
-          id: 'homepage.feature.easy-to-use.description',
-        })
-      </>
+      <Translate id="homepage.feature.easy-to-use.description">
+        为了更快地开发和部署，我们准备了文档以及 Docker 镜像，同时也搭建了相应的镜像站来加快访问速度。
+      </Translate>
     ),
   },
   {
     title: translate({ message: '可拓展化', id: 'homepage.feature.expandable.title' }),
     description: (
-      <>
-        translate({
-          message: '我们提供了插件拓展功能，你可以为 python-openbmclapi 编写自己的插件。',
-          id: 'homepage.feature.expandable.description',
-        })
-      </>
+      <Translate id="homepage.feature.expandable.description">
+        我们提供了插件拓展功能，你可以为 python-openbmclapi 编写自己的插件。
+      </Translate>
     ),
   },
   {
     title: translate({ message: '轻量化', id: 'homepage.feature.lightweight.title' }),
     description: (
-      <>
-        translate({
-          message: '基于 Python 强大的语言功能，编写出来的 python-openbmclapi 可以减小空间和内存的占用。',
-          id: 'homepage.feature.lightweight.description',
-        })
-      </>
+      <Translate id="homepage.feature.lightweight.description">
+        基于 Python 强大的语言功能，编写出来的 python-openbmclapi 可以减小空间和内存的占用。
+      </Translate>
     )
   }
 
