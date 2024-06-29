ac9e1f6e3181c5dcb359fa789ae0ff761d2d5022
bugungu <113701655+SilianZ@users.noreply.github.com>
2024-06-29 13:02:04 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,8 +116,8 @@ const config = {
             title: '更多',
             items: [
               {
-                html: `<p>Powered by <a href="https://vercel.com/" className="flex items-center"><img height="22" src="./img/vercel.svg" /></a></p>
-                     <p>Build with <a href="https://docusaurus.io/"><img height="32" src="./img/docusaurus.svg" /> <b>Docusaurus</b></a></p>`,
+                html: `<p>Powered by <a href="https://vercel.com/"><img height="18" src="/img/vercel.svg" /></a></p>
+                     <p>Build with <a href="https://docusaurus.io/"><img height="25" src="/img/docusaurus.svg" /> <b>Docusaurus</b></a></p>`,
               }, 
             ],
           },
